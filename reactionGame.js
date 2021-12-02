68c31ba42a6588a8c32fdeb1f0e2d438a591a689
Emma Drew <emmadrew@live.unc.edu>
2021-12-02 16:13:08 -0500

    logic of button game is updated

--- reactionGame.js
+++ reactionGame.js
@@ -1,28 +1,28 @@
-var click; var start; var reaction;
+			// variables for monitoring time
+			var click; var start; var react; 
+		
+			// generates a new red cirle and begins the timing process again
+			function makeCircle() {
+					var time=Math.random();
+					time=time*5000;
+				
+				// the circle will change to green after a random amount of time
+				setTimeout(function() {
+					document.getElementById("circle").style.backgroundColor="green";
+					start=Date.now();
+				}, time); 
+			}
+			
+			// when someone clicks the button, it will record their reaction time, change the color of the button back to red, and restart
+			document.getElementById("circle").onclick=function() {
+				click=Date.now();
+				react=(click-start)/1000;
+				document.getElementById("printReactionTime").innerHTML="Your Reaction Time is: " + react + "seconds";
+				document.getElementById("circle").style.backgroundColor="red";
 
-console.log("hey");
-
-// function circleGenerator() {
-//     var icon = document.getElementById("circlegame");
-
-//     var randomTime = (Math.random() * 0.456);
-//     start=Date.now();
-//     setTimeout(changeColor(), randomTime);
-// }
-
-// document.getElementById("circlegame").onclick=function() {
-//     click=Date.now();
-//     reaction=(click - start)/1000;
-//     document.getElementById("circlegame").style.backgroundColor("#00FF00");
-// }
-
-// function changeColor() {
-//     document.getElementById("circlegame").style.backgroundColor="#00FF00";
-// }
-
-// function getTime() {
-//     click = Date.now();
-// }
-
-//     document.getElementById("reaction").innerHTML="HI aubrie" + reaction;
-//     this.style.background="blue";
+				// calls the make circle function to recycle the process
+				makeCircle();
+			}
+			
+			// calls the make circle function to begin the entire process
+			makeCircle(); 
