2c0ef899f35a749914fb8b0100677f7bdbbf5769
aubriebarnhart <aubrieb@live.unc.edu>
2021-12-02 15:10:51 -0500

    added my code from A04 for functionality of buttons

--- server.js
+++ server.js
@@ -9,31 +9,34 @@ var md5 = require("md5")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set server port
-var HTTP_PORT = 5000
-// Start server
-app.listen(HTTP_PORT, () => {
-	console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+// CREATE a new user (HTTP method POST)
+app.post("/app/new",  (req, res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message": info.changes+ " record created: ID " +info.lastInsertRowid + " (201)"});
 });
-// READ (HTTP method GET) at root endpoint /app/
-app.get("/app/", (req, res, next) => {
-	res.json({ "message": "Your API works! (200)" });
+// READ a list of all users (HTTP method GET)
+app.get("/app/users", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo").all();
+	res.status(200).json(stmt);
+});
+
+// READ a single user (HTTP method GET) 
+app.get("/app/user/:id", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
+	res.json(stmt);
 	res.status(200);
 });
-	
-
-// Define other CRUD API endpoints using express.js and better-sqlite3
-// CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new/", (req, res, next) => {
-	const user = {user: req.body.user, pass: req.body.pass ? md5(req.body.pass): null}
-	const stmt = db.prepare('INSERT INTO userinfo (user,pass) VALUES(?, ?)')
-	const output = stmt.run(user.user, user.pass)
-	res.status(201).json({"message": output.changes + " record created: ID " + output.lastInsertRowid + " (201)"})
-}
-);
-
-// READ a list of all users (HTTP method GET) at endpoint /app/users/
-app.get("/app/users", (req, res) => {
-	const stmt = db.prepare('SELECT * FROM userinfo').all();
-	res.status(200).json(stmt);
+// UPDATE a single user (HTTP method PATCH)
+app.patch("/app/update/user/:id", (req, res) => {	
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?"
+	);
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(200).json({"message": info.changes+ " record updated: ID " +req.params.id + " (200)"});
+});
+// DELETE a single user (HTTP method DELETE)
+app.delete("/app/delete/user/:id", (req, res) => {	
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
+	const info = stmt.run(req.params.id);
+	res.status(200).json({"message": info.changes+ " record deleted: ID " +req.params.id + " (200)"});
 });
