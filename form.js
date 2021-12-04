8bbe8669dd1c2a18d9ad1ddd8f29354ecd08b490
Peter Romero <peter@Peters-MacBook-Pro-2.local>
2021-12-03 22:12:24 -0500

    Added script element linking form.js to all html files and worked on making the form.js work

--- form.js
+++ form.js
@@ -9,23 +9,38 @@ window.addEventListener("load", function() {
 
   function loginFunction() {
     const dataLoginForm = new FormData(login);
-    const inforLoginForm = new URLSearchParams(dataLoginForm);
+    const infoLoginForm = new URLSearchParams(dataLoginForm);
     
-    const usernameInput = dataLoginForm.get("username");
-    const passwordInput = dataLoginForm.get("password");
+    //const usernameInput = dataLoginForm.get("username");
+    //const passwordInput = dataLoginForm.get("password");
+
+    const usernameInput = infoLoginForm.get("username");
+    const passwordInput = infoLoginForm.get("password");
 
     const checkUsername = new XMLHttpRequest();
-    checkUsername.open("GET", "http://localhost:5000/app/user/usernameInput");
+    checkUsername.open("GET", "http://localhost:5000/app/user/" + usernameInput);
     checkUsername.send();
 
-    if (checkUsername.status == 404) {
+    checkUsername.addEventListener("load", function(event){
+      if (checkUsername.status == 404) {
+        alert("Username is incorrect.");
+      } else {
+        const checkPassword = JSON.parse(checkUsername.response).pass;
+        if (passwordInput != checkPassword) {
+          alert("Password is incorrect.")
+        }
+      }
+    })
+      
+
+    /*if (checkUsername.status == 404) {
       alert("Username is incorrect.");
     } else {
       const checkPassword = JSON.parse(checkUsername.response).pass;
-      if (passwordInput == checkPassword) {
-        
+      if (passwordInput != checkPassword) {
+        alert("Password is incorrect.")
       }
-    }
+    }*/
 
   }
 
