1b2af6a70162135d83565e4a284c894ce29a1371
Peter Romero <peter@Peters-MacBook-Pro-2.local>
2021-12-04 23:04:36 -0500

    changes for database and server functionality

--- database.js
+++ database.js
@@ -7,8 +7,8 @@ const Database = require('better-sqlite3');
 const db = new Database('user.db');
 
 // Is the database initialized or do we need to initialize it?
-const stmt = db.prepare('SELECT * FROM userinfo')
-// const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
+//const stmt = db.prepare('SELECT * FROM userinfo')
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
 let row = stmt.get();
 if (row === undefined) {
 // Echo information about what you are doing to the console.
